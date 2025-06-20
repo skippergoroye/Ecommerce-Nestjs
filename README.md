c9e695d5819f400d83bfd4724e4b576d6fa3c24b
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-06-20 16:06:26 +0100

    feat: migrations added

---
+++ README.md
@@ -0,0 +1,39 @@
+# Installations
+1. npm install --save @nestjs/typeorm typeorm pg (typeorm postgres)
+# https://docs.nestjs.com/techniques/database
+
+
+
+# https://docs.nestjs.com/techniques/database#async-configuration
+
+
+2. npm i --save @nestjs/config (for dotenv)
+https://docs.nestjs.com/techniques/configuration
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Chatgpt Command For Migration
+npm run build
+npm run migration:generate -- src/migrations/AddDescriptionFiveToTask
+npm run migration:run
+
---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes the configuration available globally
+      envFilePath: '.env', // Path to the environment variables file
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
