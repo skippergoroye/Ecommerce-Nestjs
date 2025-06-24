6e5e796dc5b86702af561604c173135a764362e7
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-06-24 08:18:16 +0100

    feat: jwt automatically decoded

--- src/cores/guards/auth.gaurds.ts
+++ src/cores/guards/auth.gaurds.ts
@@ -1,17 +1,30 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
+
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  canActivate(
+  constructor(private jwtService: JwtService) {}
+  async canActivate(
     context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  ):  Promise<boolean>  {
     const request = context.switchToHttp().getRequest();
 
     // Extract the token from the Authorization header
 
-    console.log(request.headers);
+    // console.log(request.headers);
+
+    const token = request.headers?.authorization.split(' ')[1];
+    // console.log(token);
     // verify  Jwt Payload
+    const currentUser = await this.jwtService.verifyAsync(token, {
+      secret: process.env.JWT_SECRET_KEY,
+    });
+
+
+    console.log("Check Current User", currentUser);
+
+    request.user = currentUser;
     return true;
   }
 }
