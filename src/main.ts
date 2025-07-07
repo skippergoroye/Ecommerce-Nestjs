3aea96b319f81cd73c8680c02519b467005efd0d
skippergoroye <oluwatosinadegoroye21@gmail.com>
2025-07-07 11:50:29 +0100

    feat: getting all section in the database

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import { getAllRoutes } from './utils/app.utils';
+import { Endpoint, HttpMethod } from './endpoint/entities/endpoint.entity';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,18 +14,35 @@ async function bootstrap() {
     credentials: true,
   });
 
+  // Start the server first so the routes are available
+  await app.listen(process.env.PORT ?? 3000);
+
+  const server = app.getHttpServer();
+  const router = server._events.request._router;
+
+  const { routes } = getAllRoutes(router);
+
   const dataSource = app.get(DataSource);
 
   try {
     console.log('Attempting to truncate table...');
     await dataSource.query('TRUNCATE endpoint RESTART IDENTITY CASCADE');
-    console.log('Truncate successfully');
+
+    for (const route of routes) {
+      const [method, url] = route.split(' ');
+      await dataSource
+        .createQueryBuilder()
+        .insert()
+        .into(Endpoint)
+        .values({ url, method: method as HttpMethod })
+        .execute();
+    }
+
+    console.log('Insert all routes into DB Successfully!');
   } catch (error) {
-    console.error('Failed to truncate table:', error);
+    console.error('Failed to truncate or insert routes:', error);
   }
 
-  await app.listen(process.env.PORT ?? 3000);
   console.log(`Application is running on: ${process.env.PORT ?? 3000}`);
 }
 bootstrap();
-
